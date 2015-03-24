940f7b0e4b0c34effe494f86e2ea2f969473f66e
rickgene <liu.rick.cn@gmail.com>
2015-03-24 15:24:59 -0600

    V3.1.0:Impl Date Class

---
+++ Bacpfsec_V2/State.cc
@@ -0,0 +1,49 @@
+/**
+ * @file   State.cc
+ * @Author Bacpfsec Rick
+ * @date   20150324
+ * @brief  Implementation of State Class
+ *
+ * State Class is used for Task Class
+ */
+
+#include "State.h"
+
+State::State() : content(""), date(Date()) {
+
+}
+
+State::State(string s, const Date& dt) : content(s), date(dt) {
+
+}
+
+State::State(const State& st) {
+  content = st.content;
+  date = st.date;
+}
+
+State::~State() {
+
+}
+
+void State::setDate(const Date& dt) {
+  date = dt;
+}
+
+Date State::getDate() {
+  return date;
+}
+
+  /**    Setter for content
+   *     @param s    A string for progress detail
+   */ 
+void State::setContent(string s) {
+  content = s;
+}
+
+  /**    Getter for content
+   *     @return      A string with details from private content
+   */ 
+string State::getContent() {
+  return content;
+}
---
+++ Bacpfsec_V2/State.h
@@ -0,0 +1,74 @@
+/**
+ * @file   State.h
+ * @Author Bacpfsec Rick
+ * @date   20150324
+ * @brief  Header file of State Class
+ *
+ * State Class is used for Task Class
+ */
+#ifndef STATE_H
+#define STAET_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "Date.h"
+
+/**
+ * @brief  State Class is used to record a progress of a task
+ * 
+ * State Class records both progress detail and performing date.
+ * State Class also provides some basic operations related with a progress.
+ *
+ */
+class State {
+public:
+  /**    Default constructor
+   *     
+   */ 
+  State();
+
+  /**    Parameterized constructor
+   *     @param s    string content for a progress
+   *     @param dt   a Date object
+   */ 
+  State(string s, const Date& dt);
+
+  /**    Copy constructor
+   *     
+   */ 
+  State(const State& st);
+
+  /**    Destructor
+   *
+   */ 
+  ~State();
+
+  /**    Setter for date
+   *     @param dt    A Date object
+   */ 
+  void setDate(const Date& dt);
+
+  /**    Getter for date
+   *     @return      A Date object with details from private date
+   */ 
+  Date getDate();
+
+  /**    Setter for content
+   *     @param s    A string for progress detail
+   */ 
+  void setContent(string s);
+
+  /**    Getter for content
+   *     @return      A string with details from private content
+   */ 
+  string getContent();
+
+
+private:
+  /** Storage of progress detail */ 
+  string content;
+
+  /** Storage of date value */ 
+  Date date;
+};
+#endif
